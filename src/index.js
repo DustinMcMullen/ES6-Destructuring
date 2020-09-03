64d01fed775be5e54b8214d609f0f647f616487a
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-09-02 20:55:45 -0500

    Destructured all Objects to render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import cars from "./practice";
+// import animals from "./data";
+// const [cat, dog] = animals;
+// const {name: catName, sound: catSound} = cat;
+// const {name: dogName, sound: dogSound} = dog;
+// console.log (dogSound);
+
+console.log(cars);
+
+const [honda, tesla] = cars;
+const {
+  hondaModel,
+  colorsByPopularity: hondaColorsByPopularity,
+  speedStats: hondaSpeedStats
+} = honda;
+const {
+  teslaModel,
+  colorsByPopularity: teslaColorsByPopularity,
+  speedStats: teslaSpeedStats
+} = tesla;
+const [hondaTopColor, hondaSecondColo] = hondaColorsByPopularity;
+const [teslaTopColor, teslaSecondColo] = teslaColorsByPopularity;
+const {
+  topSpeed: hondaTopSpeed,
+  zeroToSixty: hondaZeroToSixty
+} = hondaSpeedStats;
+const {
+  topSpeed: teslaTopSpeed,
+  zeroToSixty: teslaZeroToSixty
+} = teslaSpeedStats;
 
 ReactDOM.render(
   <table>
@@ -10,12 +40,12 @@ ReactDOM.render(
     <tr>
       <td>{tesla.model}</td>
       <td>{teslaTopSpeed}</td>
-      <td>{teslaTopColour}</td>
+      <td>{teslaTopColor}</td>
     </tr>
     <tr>
       <td>{honda.model}</td>
       <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td>
+      <td>{hondaTopColor}</td>
     </tr>
   </table>,
   document.getElementById("root")
